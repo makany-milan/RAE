f1fa572f74ed01f079cb257b345e7e8ffb19747c
Milan Makany <ecudpb@godzilla.csc.warwick.ac.uk>
2023-08-28 11:58:22 +0100

    New cluster scripts

--- cluster_scripts/filter_econ_authors_complete_works.sh
+++ cluster_scripts/filter_econ_authors_complete_works.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=rae_filter_author_works                   # Job name
+#SBATCH --job-name=rae_filter_author_works_complete                   # Job name
 #SBATCH --mail-type=ALL                                 # Mail events (NONE, BEGIN, END, FAIL, ALL) - update on ALL
 #SBATCH --mail-user=milan.makany@warwick.ac.uk          # Notifications to email
 #SBATCH --nodes=1                                       # Use one node
 #SBATCH --ntasks-per-node=1                             # Run the single python script
 #SBATCH --cpus-per-task=4                               # Allocate cores
 #SBATCH --mem-per-cpu=4000                              # Allocate memory for the job
-#SBATCH --time=4:00:00                                 # Time limit, estimated based on previous single core runs
+#SBATCH --time=16:00:00                                 # Time limit, estimated based on previous single core runs
 
 # The time limit on this script may be a large overestimate
 # Never executed this script with the new data structure
 
 # Execute code
-python3 /storage/economics/ecudpb/wia/scripts/openalex_scripts/filter.py --filter_econ_works --remote True
+python3 /storage/economics/ecudpb/wia/scripts/openalex_scripts/filter.py --filter_econ_authors_works --remote True
+
